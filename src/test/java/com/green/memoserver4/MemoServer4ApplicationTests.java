dfd216404ca71b2c1bd2a59f1194f27916cc7bf7
habi802 <shb8002@gmail.com>
2025-07-04 09:59:37 +0900

    [init] first

---
+++ src/test/java/com/green/memoserver4/MemoServer4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.memoserver4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoServer4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
